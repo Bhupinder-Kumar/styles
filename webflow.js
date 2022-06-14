f0971e12ecec21912269b293e3b0f782bfb0c56c
bhupinder.kumar <bhupinder.kumar@venturepact.com>
2022-06-14 10:56:17 +0530

    cta functionality fixes

--- webflow.js
+++ webflow.js
@@ -1,34 +1,57 @@
 // *******------**********
 // Js File for Webflow
-$(document).ready(function(){      
+$(document).ready(function () {
     $(".nav-header-dashboard-3").css("visibility", "hidden");
 
-$(".nav-link-3").mouseover(function(){
-    $(".nav-header-dashboard-3").css("visibility", "visible");
-});
+    $(".nav-link-3").mouseover(function () {
+        $(".nav-header-dashboard-3").css("visibility", "visible");
+    });
 
-$(".nav-header-dashboard-3").mouseleave(function(){
-    $(this).css("visibility", "hidden");
-});
+    $(".nav-header-dashboard-3").mouseleave(function () {
+        $(this).css("visibility", "hidden");
+    });
 
-$(".close-btn").click(function(){
-    $(".w-nav-overlay").css("display", "none");
-});
+    $(".close-btn").click(function () {
+        $(".w-nav-overlay").css("display", "none");
+    });
 
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-  
-  if(scroll >= 30){
-      $(".header-4").addClass("sticky");
-  }
-  else{
-      $(".header-4").removeClass("sticky");
-  }
-})
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 30) {
+            $(".header-4").addClass("sticky");
+        }
+        else {
+            $(".header-4").removeClass("sticky");
+        }
+    });
+
+    $(".video-link").click(function () {
+        $(".dd-modal").css("display", "flex");
+        $("body").css("overflow", "hidden");
+    });
+    $(".dd-modal").click(function () {
+        $(this).css("display", "none");
+        $("body").css("overflow", "auto");
+    });
 });
 
 var Webflow = Webflow || [];
 Webflow.push(function () {
-  var submitBtn = document.querySelector("input[type=submit]");
-submitBtn.disabled = true;
+    var submitBtn = document.querySelector("input[type=submit]");
+    submitBtn.disabled = true;
 });
+
+const modal = document.querySelector(".dd--cta");
+const overlay = document.querySelector(".dd-overlay");
+const open_modal = document.querySelector(".nav-link-signin");
+
+const close_Modal = function () {
+    overlay.classList.add("hidden");
+    modal.classList.add("hidden");
+}
+open_modal.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+});
+overlay.addEventListener("click", close_Modal);
